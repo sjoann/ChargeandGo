7a9aef6d40de2211ea3e1a1314a18842145fe22b
cadencjk <cadencheong@gmail.com>
2022-05-18 15:36:06 +0800

    HomeScreen display user name

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, StyleSheet, TouchableOpacity, Button,  TextInput } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Button,  TextInput, ImageBackground, useState } from 'react-native'
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import { Text,  Appbar, Provider as PaperProvider } from 'react-native-paper'
@@ -8,6 +8,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
 export default function HomeScreen({ navigation }) {
+
   const logoutUser = () => {
     try {
       firebase.auth().signOut()
@@ -18,12 +19,20 @@ export default function HomeScreen({ navigation }) {
   }
  
     return (
+      <ImageBackground style={styles.background} source={require("../components/pics/background.png")}>
         <View style= {styles.container}>
-           <Button
+          <Text style={styles.header}>
+            Welcome Back, {firebase.auth().currentUser?.displayName}
+          </Text>
+
+          <Button
                title="Log out"
                onPress={logoutUser}
+               style={styles.logOutButton}
            ></Button>
         </View>
+
+      </ImageBackground>
     )
 }
 const styles = StyleSheet.create({
@@ -31,11 +40,18 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     width: '100%',
-    maxWidth: 340,
+    marginTop: 20,
     alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  background: {
+    flex: 1
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
 })
 
 
