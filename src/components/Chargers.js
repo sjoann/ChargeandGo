1f68502c28083a89475249d2b4ff829dca4c6faf
cadencjk <cadencheong@gmail.com>
2022-05-22 16:00:01 +0800

    Map function with Firebase DB

---
+++ src/components/Chargers.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, TouchableOpacity, Button,  Text, ImageBackground, Dimensions, Image, List, FlatList } from 'react-native'
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+import { getDocs, getFirestore, collection } from 'firebase/firestore/lite'
+
+function markers(chargers) {
+    return( 
+        chargers.map((item)=> {
+            return <Marker coordinate={{
+                latitude: item.location.latitude,
+                longitude: item.location.longitude,
+            }}
+            title={item.name}
+            description={item.description}
+            >
+            <Image 
+                source={require('../components/pics/location_cropped.png')}
+                style={{width: 50, height: 50}}
+                resizeMode="contain"
+            />
+            </Marker>
+        }
+
+        )
+    )
+}
+
+
+class Chargers extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            chargersList: []
+        }
+    }
+
+
+    componentDidMount() {
+        const chargers = []
+        const db = getFirestore()
+        const colRef = collection(db, 'chargers')
+        getDocs(colRef).then((snapshot) => {
+            snapshot.docs.forEach((doc) => {
+                chargers.push({...doc.data(), id: doc.id })
+            })
+            this.setState({chargersList: chargers})
+        })
+    }
+
+    render() {
+        const { chargersList } = this.state;
+
+        return(
+            <View>
+                <MapView 
+                initialRegion={{
+                    latitude: 1.351927,
+                    longitude: 103.867081,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                style={styles.map}>       
+                    {markers(chargersList)}
+                </MapView>
+                <FlatList 
+                    data={chargersList}
+                    renderItem={
+                        ({item}) => <Text style={styles.item}>
+                            {item.name}
+                            {"  "}
+                            {item.description}
+                        </Text>
+                    }
+                
+                />
+            </View>
+                
+                
+            );
+    }
+}
+
+const styles = StyleSheet.create({
+    map: {
+        width: Dimensions.get('window').width,
+        height: 400,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+
+})
+
+export default Chargers;
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,70 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, StyleSheet, TouchableOpacity, Button,  TextInput, ImageBackground, Dimensions, Image } from 'react-native'
-import { Text } from 'react-native-paper'
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import BackButton from '../components/BackButton';
+import Chargers from '../components/Chargers'
 
-export default function MapScreen() {
-    
+
+export default function MapScreen({ navigation }) {
 
     return(
-    <ImageBackground style={styles.background} source={require("../components/pics/background.png")}>
-        <View style={styles.container}>
-            <MapView initialRegion={{
-                latitude: 1.351927,
-                longitude: 103.867081,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}
-            style={styles.map}>
-
-                <Marker coordinate={{
-                        latitude: 1.380,
-                        longitude: 103.867081,
-                    }}
-                    title="charger #1"
-                    description="this is a charger"
-                    >
-                    <Image 
-                        source={require('../components/pics/location_cropped.png')}
-                        style={{width: 50, height: 50}}
-                        resizeMode="contain"
-                    />
-                </Marker>
-                <Marker coordinate={{
-                        latitude: 1.351927,
-                        longitude: 103.9,
-                    }}
-                    title="charger #2"
-                    description="this is a charger"
-                    >
-                    <Image 
-                        source={require('../components/pics/location_cropped.png')}
-                        style={{width: 50, height: 50}}
-                        resizeMode="contain"
-                    />
-                </Marker>
-            </MapView>
-        </View>
-    </ImageBackground>
+        <ImageBackground style={styles.background} source={require("../components/pics/background.png")}>
+            <View style={styles.container}>
+                <BackButton goBack={ navigation.goBack }/>
+                <Chargers />
+            </View>
+        </ImageBackground>
     );
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    screenText:{
-        color: '#FFFFFF', 
-        fontSize: 30,
-        textAlign: 'center'
-    },
     background: {
         flex: 1
     },
-    map: {
-        width: Dimensions.get('window').width,
-        height: 300,
-    }
 })
